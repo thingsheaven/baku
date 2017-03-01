a7f5d6ac41570df3f7b3b0bc485d9acb033db224
anthony <anthony.chinwo@gmail.com>
2017-03-01 21:28:04 +0000

    - copy/paste from endodb repo

---
+++ README.md
@@ -0,0 +1,2 @@
+# endodb
+Service provider-independent object database that can be backed by Firebase real-time database, Mongdb or traditional relational databases.
---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let EventEmiter = require('events');
+
+class IEndoDbClient {
+    
+}
+
+class EndoDbModel {
+    
+}
+
+class EndoDb extends EventEmiter {
+    
+    constructor(client, options={}){
+        super()
+    }
+    
+    new(modelCls, ){
+        
+    }
+}
